42f2198df22b96d2b51cc7b47311072bb2bbf6e3
AllySike <mn1111@mail.ru>
2021-02-15 20:13:03 +0300

    terjyuy

--- mlx/draw.c
+++ mlx/draw.c
@@ -24,7 +24,29 @@ int		create_trgb(int t, int r, int g, int b)
 {
 	return(t << 24 | r << 16 | g << 8 | b);
 }
-int main ()
+#include <mlx.h>
+
+typedef struct  s_vars {
+    void        *mlx;
+    void        *win;
+}               t_vars;
+
+int             key_hook(int keycode, t_vars *vars)
 {
-	
+    printf("Hello from key_hook!\n");
 }
+
+int             main(void)
+{
+    t_vars      vars;
+    int y = 0;
+    int x = 0;
+
+    vars.mlx = mlx_init();
+    vars.win = mlx_new_window(vars.mlx, 650, 400, "Hello world!");
+    mlx_key_hook(vars.win, key_hook, &vars);
+    mlx_loop(vars.mlx);
+    while (x < 320)
+        while (y < 400)
+            mlx_pixel_put(vars.mlx, vars.win, x, y, create_trgb(2, 100, 255, 200));
+} 
